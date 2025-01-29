9c1b46b795349e57573053b76221b5216088d24d
Betul <betulkoru06@gmail.com>
2025-01-29 12:31:28 +0300

    feat: js input dec

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 
     const fehBody = document.body;
 
+    const workDurationInput = document.getElementById('work-duration');
+    const restDurationInput = document.getElementById('rest-duration');
+
     window.addEventListener('load', () => {
         fehBody.classList.add('page-loaded');
     });
