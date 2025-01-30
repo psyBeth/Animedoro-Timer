ce0e263cc4b7a45819523a94d666cd7d446d8f64
Betul <betulkoru06@gmail.com>
2025-01-30 12:15:45 +0300

    feat: work duration

--- main.js
+++ main.js
@@ -4,6 +4,9 @@
 
     const workDurationInput = document.getElementById('work-duration');
     const restDurationInput = document.getElementById('rest-duration');
+    const timerTime = document.getElementById('timer-time');
+
+    let workDuration = parseInt(workDurationInput.value) * 60;
 
     window.addEventListener('load', () => {
         fehBody.classList.add('page-loaded');
