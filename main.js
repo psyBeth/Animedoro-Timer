1b1427f3488389e2a134ec0ad7dab99d04328876
Betul <betulkoru06@gmail.com>
2025-02-04 10:46:04 +0300

    feat: timer update func

--- main.js
+++ main.js
@@ -22,9 +22,11 @@
     const startBtn = document.getElementById('start-btn');
 
     startBtn.addEventListener('click', () => {
+
         isPaused = false;
 
         fehBody.classList.add('timer-running');
+
         if(isWorking) {
             fehBody.classList.remove('timer-paused');
         } else {
@@ -35,5 +37,15 @@
             intervalId = setInterval(updateTimer, 1000);
         }
     });
+
+    //* Update Timer
+    function updateTimer() {
+
+        if(!isPaused) {
+            
+            remainingTime--;
+
+        }
+    }
 
 })();
