1bf8a85bf214d12a1ac275a137292ac73e7f7bed
Betul <betulkoru06@gmail.com>
2025-02-07 10:35:51 +0300

    feat: format time func

--- main.js
+++ main.js
@@ -77,4 +77,10 @@
         timerTime.textContent = formatTime(remainingTime);
     }
 
+    function formatTime(seconds) {
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = seconds % 60;
+        return `${minutes.toString().padStart(2, '0')} : ${remainingSeconds.toString().padStart(2, '0')}`;
+    }
+
 })();
